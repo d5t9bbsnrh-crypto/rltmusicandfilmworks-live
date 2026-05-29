f037868bea87b79c5a151461e915cffe6581e45b
T. Alan Evans <283194095+d5t9bbsnrh-crypto@users.noreply.github.com>
2026-05-28 22:46:35 -0700

    Use static Astro build for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
+cat > astro.config.mjs <<'EOF'
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: cloudflare()
-});
+export default defineConfig({});
+EOF
